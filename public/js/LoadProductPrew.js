e970214971dc818e94cdc2272885b4561313ddc7
Axe6365 <axe6365@gmail.com>
2025-11-03 21:17:32 +0100

    v0.01

---
+++ public/js/LoadProductPrew.js
@@ -0,0 +1,38 @@
+async function loadProducts() {
+      try {
+        const response = await fetch('http://localhost:3000/api/products');
+        const products = await response.json();
+
+        // tároljuk a termékeket a böngészőben (localStorage)
+        localStorage.setItem('products', JSON.stringify(products));
+
+        // megjelenítés
+        const grid = document.getElementById('product-grid');
+        grid.innerHTML = ''; // előző tartalom törlése
+        let i = 0;
+    products.forEach(p => {
+        if(i < 3) return;
+        i++;
+      const card = document.createElement('div');
+      card.classList.add('product');
+
+      card.innerHTML = `
+        <div class="product-media placeholder">
+          <img src="media/szeles.jpeg" width="100%" height="110%" alt=""><!--p.img_url-->
+        </div>
+        <div class="product-body">
+          <h4>${p.name}</h4>
+          <p class="muted">${p.description}</p>
+          <div class="product-actions">
+            <span class="price">€${p.price}</span>
+            <button class="btn btn-sm">Details</button>
+          </div>
+        </div>
+      `;
+      grid.appendChild(card);
+});
+      } catch (err) {
+        console.error('❌ Hiba a lekéréskor:', err);
+      }
+    }
+addEventListener("DOMContentLoaded",loadProducts)
